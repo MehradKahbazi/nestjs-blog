c473c764ded690d2cdb88b3ffef9b87f02721015
Mehrad <mehradhetfield@gmail.com>
2024-12-17 18:20:49 +0330

    fix: dto added to users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,17 +11,17 @@ import {
   DefaultValuePipe,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(limit);
-    console.log(page);
+    console.log(getUsersParamDto);
     return 'You sent a get request to users endpoint';
   }
 
