5668c59714364dede0c3d073d0781f62c1cc9f92
Mehrad <mehradhetfield@gmail.com>
2024-12-17 18:14:20 +0330

    fix: dto typo fix

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -24,5 +24,5 @@ export class CreateUserDto{
         message:
           'Minimum eight characters, at least one letter, one number and one special character',
       })
-    passsword: string;
+    password: string;
 }
