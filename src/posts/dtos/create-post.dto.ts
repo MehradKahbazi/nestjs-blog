21d9a85a156ff5848cdeeae11e56c3306879023e
Mehrad <mehradhetfield@gmail.com>
2024-12-18 16:55:36 +0330

    feat: nested dtos

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,15 +1,56 @@
+import { IsArray, IsEnum, IsISO8601, IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength, ValidateNested } from 'class-validator';
 import { postStatus } from '../enums/postStatus.enum';
 import { postType } from '../enums/postType.enum';
+import { CreateProstMetaOptionsDto } from './create-post-meta-options.dto';
+import { Type } from 'class-transformer';
 
 export class CreatePostDto {
+  @IsString()
+  @MinLength(4)
+  @IsNotEmpty()
   title: string;
+
+  @IsEnum(postType)
+  @IsNotEmpty()
   postType: postType;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message:
+      'A slug should be all small letters and uses only "-" and without spaces. For example "my-url"',
+  })
   slug: string;
+
+  @IsEnum(postStatus)
+  @IsNotEmpty()
   status: postStatus;
+
+  @IsString()
+  @IsOptional()
   constent?: string;
+
+  @IsJSON()
+  @IsOptional()
   schema?: string;
+
+  @IsUrl()
+  @IsOptional()
   featuredImageUrl: string;
+
+  @IsISO8601()
+  @IsOptional()
   publishedOn?: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({each: true})
+  @MinLength(3, {each: true})
   tags?: string[];
-  metaOptions?: [{key: 'sidebarEnabled'; value: 'true'}];
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({each: true})
+  @Type(() => CreateProstMetaOptionsDto)
+  metaOptions?: CreateProstMetaOptionsDto[];
 }
