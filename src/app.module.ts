80d796279268e58e8b3140a0383779f11b18dc71
Mehrad <mehradhetfield@gmail.com>
2024-12-21 22:39:55 +0330

    feat: users entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,23 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [UsersModule, PostsModule,
-  TypeOrmModule.forRoot({
-    type: 'postgres',
-    entities:[],
-    synchronize: true,
-    port: 5432,
-    username: 'postgres',
-    password: 'Metallica1993',
-    host: 'localhost',
-    database: 'nestjs-blog'
+  TypeOrmModule.forRootAsync({
+    imports:[],
+    inject:[],
+    useFactory: () => ({
+      type: 'postgres',
+      entities:[User],
+      synchronize: true,
+      port: 5432,
+      username: 'postgres',
+      password: 'Metallica1993',
+      host: 'localhost',
+      database: 'nestjs-blog'
+    })
   })
   ] 
 })
