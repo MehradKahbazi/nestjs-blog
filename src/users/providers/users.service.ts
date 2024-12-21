874a23536c2c4907863040d011b2c8b220b78b42
Mehrad <mehradhetfield@gmail.com>
2024-12-21 22:59:28 +0330

    ci: injecting repository

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user.entity';
+import { Repository } from 'typeorm';
 /**
  * Class to connect to users table and perform business operations
  */
 @Injectable()
 export class UsersService {
+
+    constructor(
+        /**
+         * Injecting usersRepository
+         */
+        @InjectRepository(User)
+        private usersRepository: Repository<User>
+    ){}
 
     /**
      * method to get all the users from database
