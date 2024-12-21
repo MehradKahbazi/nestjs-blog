1c0f9beaaf483a59a7270826d734f86bb7d88b9e
Mehrad <mehradhetfield@gmail.com>
2024-12-21 22:26:24 +0330

    feat: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule] 
+  imports: [UsersModule, PostsModule,
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    entities:[],
+    synchronize: true,
+    port: 5432,
+    username: 'postgres',
+    password: 'Metallica1993',
+    host: 'localhost',
+    database: 'nestjs-blog'
+  })
+  ] 
 })
 export class AppModule {}
