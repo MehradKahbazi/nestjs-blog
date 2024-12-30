9bc4e3411b7867fb28e6e0a230c4fffd680f4c48
Mehrad <mehradhetfield@gmail.com>
2024-12-31 00:46:37 +0330

    feat: tags repository injected

--- src/tags/providers/tags.service.ts
+++ src/tags/providers/tags.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTagDto } from '../dtos/create-tag.dto';
+import { Repository } from 'typeorm';
+import { Tag } from '../tag.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TagsService {}
+export class TagsService {
+    constructor(
+        /**
+         * inject tagsRepository
+         */
+        @InjectRepository(Tag)
+        private readonly tagsRepository: Repository<Tag>
+    ){}
+    public async create(createTagDto: CreateTagDto){
+    }
+}
